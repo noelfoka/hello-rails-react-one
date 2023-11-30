133038e5dfbccb8733ef824123d3fdf81cafbeb3
noelfoka <nomgnenoel@gmail.com>
2023-12-01 00:28:48 +0100

    add linters file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 greetings = [
-  { content: "Bonjour!" },
-  { content: "Salut!" },
-  { content: "Coucou!" },
-  { content: "Bienvenue!" },
-  { content: "Salutations!" }
+  { content: 'Bonjour!' },
+  { content: 'Salut!' },
+  { content: 'Coucou!' },
+  { content: 'Bienvenue!' },
+  { content: 'Salutations!' }
 ]
 
 greetings.each do |greeting|
--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class ApiControllerTest < ActionDispatch::IntegrationTest
-  test "should get random_greeting" do
+  test 'should get random_greeting' do
     get api_random_greeting_url
     assert_response :success
   end
