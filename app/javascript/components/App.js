8e8e2eda3746b30e981dc053ea336d8941d06979
noelfoka <nomgnenoel@gmail.com>
2023-12-01 00:05:36 +0100

    add redux

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,24 @@
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getGreetings } from '../redux/greetings/greetingSlice';
+
+import Greeting from './Greeting';
+
 const App = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
+  );
 }
 
 export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'api/random_greeting'
   root 'home#index'
+
+  namespace :api do
+    get '/random_greeting', to: 'api#random_greeting'
+  end
 end
